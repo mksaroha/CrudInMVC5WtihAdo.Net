a018d5dccebf0a20deec3310296da993b90e6efb
mksaroha <mksaroha@hotmail.com>
2022-07-12 13:08:44 +0530

    fix bug #1

--- CrudWithAdo/Repository/StudentRepository.cs
+++ CrudWithAdo/Repository/StudentRepository.cs
@@ -35,31 +35,31 @@ namespace CrudWithAdo.Repository
             return allStudents;
 			
 			
-			List<Emplyee> employeeList = new List<Emplyee>();  
-            string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;  
-            using (SqlConnection con = new SqlConnection(CS))  
-            {  
-                SqlCommand cmd = new SqlCommand("SELECT * FROM Employees", con);  
-                cmd.CommandType = CommandType.Text;  
-                con.Open();  
+			//List<Emplyee> employeeList = new List<Emplyee>();  
+   //         string CS = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;  
+   //         using (SqlConnection con = new SqlConnection(CS))  
+   //         {  
+   //             SqlCommand cmd = new SqlCommand("SELECT * FROM Employees", con);  
+   //             cmd.CommandType = CommandType.Text;  
+   //             con.Open();  
    
-                SqlDataReader rdr = cmd.ExecuteReader();  
-                while (rdr.Read())  
-                {  
-                    var employee = new Emplyee();  
+   //             SqlDataReader rdr = cmd.ExecuteReader();  
+   //             while (rdr.Read())  
+   //             {  
+   //                 var employee = new Emplyee();  
    
-                    employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);  
-                    employee.Name = rdr["Name"].ToString();  
-                    employee.Gender = rdr["Gender"].ToString();  
-                    employee.Age = Convert.ToInt32(rdr["Age"]);  
-                    employee.Position = rdr["Position"].ToString();  
-                    employee.Office = rdr["Office"].ToString();  
-                    employee.HireDate =Convert.ToDateTime(rdr["HireDate"]);  
-                    employee.Salary = Convert.ToInt32(rdr["Salary"]);  
-                    employeeList.Add(employee);  
-                }  
-            }  
-            return View(employeeList);   
+   //                 employee.EmployeeId = Convert.ToInt32(rdr["EmployeeId"]);  
+   //                 employee.Name = rdr["Name"].ToString();  
+   //                 employee.Gender = rdr["Gender"].ToString();  
+   //                 employee.Age = Convert.ToInt32(rdr["Age"]);  
+   //                 employee.Position = rdr["Position"].ToString();  
+   //                 employee.Office = rdr["Office"].ToString();  
+   //                 employee.HireDate =Convert.ToDateTime(rdr["HireDate"]);  
+   //                 employee.Salary = Convert.ToInt32(rdr["Salary"]);  
+   //                 employeeList.Add(employee);  
+   //             }  
+   //         }  
+   //         return View(employeeList);   
 
         }
 
